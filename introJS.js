f992afc7f6894907b1a8310676233c15df387007
Matthew Duck <MatthewDDuck@googlemail.com>
2022-05-18 13:00:40 +0100

    Day3 Morning

---
+++ introJS.js
@@ -0,0 +1,11 @@
+console.log("Matthew");
+console.info("Duck");
+console.count("Canterbury");
+console.assert("Dunno");
+
+let make = "Toyota";
+let model = "Aygo";
+console.log("My favourite car is a " + make + " " + model);
+console.log(`My favourite car is a ${make} ${model}`);
+
+console.log("%c My favourite car is a toyota aygo","color:orange ; font-family:fantasy ; font-style:Bold ; background-color:black ; padding:10px");
